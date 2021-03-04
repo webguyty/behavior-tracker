6b409ece8bd9ee31dc7f8105784d5be9958f23f0
Tyler <tyler@webguyty.com>
2021-03-04 15:55:31 -0800

    Starting to create user

--- src/handlers/createUser.js
+++ src/handlers/createUser.js
@@ -1,4 +1,9 @@
 async function createUser(event, context) {
+  const now = new Date();
+  const newUser = {
+    ip: "",
+    firstVisit: now.toISOString(),
+  };
   return {
     statusCode: 200,
     body: JSON.stringify({ event, context }),
