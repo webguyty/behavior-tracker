b99aaf1916e54a1e76b2aa21f821c9e880c2eb5d
Tyler <tyler@webguyty.com>
2021-03-08 15:04:50 -0800

    added dynamodb table

--- src/handlers/createUser.js
+++ src/handlers/createUser.js
@@ -31,8 +31,8 @@ async function createUser(event, context) {
   };
 
   return {
-    statusCode: 200,
-    body: JSON.stringify({ newUser }),
+    statusCode: 201,
+    body: JSON.stringify(newUser),
   };
 }
 
