ae5c1ea690587eac1f24d46bdef8b7ebba29c45d
Tyler <tyler@webguyty.com>
2021-03-09 15:44:59 -0800

    scrollspy working

--- fe/app.js
+++ fe/app.js
@@ -1,5 +1,13 @@
 const apiURL = "https://3uw8yuipx5.execute-api.us-west-2.amazonaws.com/dev";
 
+// Initializing gumshoe
+const header = document.querySelector("#my-awesome-nav");
+const spy = new Gumshoe("#my-awesome-nav a", {
+  offset: function () {
+    return header.getBoundingClientRect().height;
+  },
+});
+
 // Create user when logging into page
 fetch(`${apiURL}/createUser`, {
   method: "POST",
