b37d9893afa9650e40a3781944e56a84a97e86ba
Tyler <tyler@webguyty.com>
2021-03-10 11:18:06 -0800

    switched to and installed axios

--- fe/app.js
+++ fe/app.js
@@ -1,34 +1,47 @@
-const apiURL = "https://3uw8yuipx5.execute-api.us-west-2.amazonaws.com/dev";
 import "../node_modules/gumshoejs/dist/gumshoe.min.js";
+
+const apiURL = "https://3uw8yuipx5.execute-api.us-west-2.amazonaws.com/dev";
 
 // Initializing gumshoe
 const header = document.querySelector("#my-awesome-nav");
 const spy = new Gumshoe("#my-awesome-nav a", {
-  // offset: function () {
-  //   return header.getBoundingClientRect().height;
-  // },
   offset: 200,
 });
 
-// Create user when logging into page
-fetch(`${apiURL}/createUser`, {
-  method: "POST",
-})
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+axios
+  .post(`${apiURL}/createUser`)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+// Create object for gumshoe to send
+const divStats = {
+  div: "",
+  enterTime: "",
+  exitTime: "",
+};
+
+// When div becomes active on screen, activate Gumshoe
 
 document.addEventListener(
   "gumshoeActivate",
   (event) => {
+    // Div name
     const divID = event.detail.content.id;
+    divStats.div = divID;
 
-    if (divID !== "content") {
-      console.log("Entering " + divID);
-    }
+    // Div enter time
+    let now = new Date();
+    now = now.toISOString();
+    divStats.enterTime = now;
   },
   false
 );
 
+// When div becomes deactive on screen, deactivate Gumshoe
 document.addEventListener(
   "gumshoeDeactivate",
   (event) => {
