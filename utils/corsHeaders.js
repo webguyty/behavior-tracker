37432b0564bcd8d0f91da39505a738fdd5c4e764
Tyler <tyler@webguyty.com>
2021-04-08 16:24:32 -0700

    reactdev

--- utils/corsHeaders.js
+++ utils/corsHeaders.js
@@ -2,7 +2,7 @@ export default function corsHeaders() {
   return {
     'Access-Control-Allow-Headers':
       'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-    'Access-Control-Allow-Origin': 'http://localhost:5500',
+    'Access-Control-Allow-Origin': 'http://localhost:3000',
     'Access-Control-Allow-Methods':
       'OPTIONS,POST,PATCH,GET,PUT,HEAD,CONNECT,OPTIONS,TRACE',
     'Access-Control-Allow-Methods': 'true',
